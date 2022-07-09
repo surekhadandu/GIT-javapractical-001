b6e11c8484aea00f0c28db173c6b36f9642cf176
surekha dandu <surekha.b1s1@gmail.com>
2022-07-09 16:04:23 +0530

    adding new java file for new requirement

---
+++ TextDemo1.java
@@ -0,0 +1,15 @@
+package com.flipkart;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		if(a<b)
+			System.out.println("a is the smallest");
+		else
+			System.out.println("b is the smallest");
+	}
+
+}
